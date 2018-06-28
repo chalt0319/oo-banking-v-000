f86ba62755635c8f854b20bc47f40c6aa8d8b408
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-28 14:44:35 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,10 @@
 class Transfer
+  attr_accessor :sender, :reciever, :amount
   
   def initialize(sender, reciever, amount)
+    @sender = sender
+    @reciever = reciever 
+    @amount = amount 
     
   end 
 end
