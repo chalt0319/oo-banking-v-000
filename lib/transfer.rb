ec3edb67899b1c6d044e2e31cfcc2688a2f8201b
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-28 14:55:57 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -22,6 +22,7 @@ class Transfer
       @receiver.deposit(@amount)
       @status = "complete"
     else 
+      "Transaction rejected. Please check your account balance."
       @status = "rejected"
     end 
   end 
