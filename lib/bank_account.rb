0b5b6f8810d4138fae02864f43f6eeda8ce8f7c0
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-28 14:37:32 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,9 @@
 class BankAccount
-  attr_accessor :name 
+  attr_accessor :name, :balance, :status 
   
   def initialize(name)
     @name = name 
+    @balance = 1000
+    @status = "open"
   end 
 end
