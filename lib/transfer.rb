039680629c75e14901f63f695a5797fffcf3cc09
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-28 15:07:22 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   end 
   
   def execute_transaction
-    if self.valid? == true || @status == "pending"
+    if self.valid? == true && @status == "pending"
       @sender.withdrawl(@amount)
       @receiver.deposit(@amount)
       @status = "complete"
